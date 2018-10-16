0b39fb1a2cee5aaf95b1809f72ca9f5de1bf3835
Kasey Martin <kasey.hackspace@gmail.com>
2018-10-17 04:37:00 +0800

    sample of using command line arguments

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 /*
-Basic first program
+Demo of command line arguments
 */
 #include <stdio.h> //include standard IO library to have access to basic functions
 
 // main function: what gets executed when you run the program
 int main(int argc, char *argv[]){ // argc - number of arguments, argv array of command line arguments
+    if(argc > 1){
+        printf("Hello %s!",argv[1]); //prints hello argv[1] to the standard output stream
+    }
+    else {
+        printf("No additional command line arguments");
+    }
     
-    printf("Hello World!"); //prints hello world to the standard output stream
-    
-    return 0; //returns '0', indicating 
+    return 0; //returns '0', indicating successful exit of program
 }
