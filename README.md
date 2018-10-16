949c3168c597b7f179d75230ecd6503b54f553e7
Kasey Martin <kasey.hackspace@gmail.com>
2018-10-17 04:29:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+This is a repository for speeding up and organizing teaching C with demo code using branches as chapters for quickly transitioning demo code.
+
+### Create new branch and switch to it
+
+`git branch -b <new_branch> <source_branch>` 
+
+### Switch to an existing branch
+
+`git branch <branch>`
+
+### List branchs/chapters
+`git branch -a`
---
+++ main.c
@@ -0,0 +1,12 @@
+/*
+Basic first program
+*/
+#include <stdio.h> //include standard IO library to have access to basic functions
+
+// main function: what gets executed when you run the program
+int main(int argc, char *argv[]){ // argc - number of arguments, argv array of command line arguments
+    
+    printf("Hello World!"); //prints hello world to the standard output stream
+    
+    return 0; //returns '0', indicating 
+}
