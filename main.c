0533f6566461d31091cf6d331b161b56425fbb5c
Kasey Martin <kasey.hackspace@gmail.com>
2018-10-17 06:02:13 +0800

    sample variable, declaration, input and output

--- main.c
+++ main.c
@@ -1,16 +1,38 @@
 /*
-Demo of command line arguments
+Demo of basic input, output, also variables
 */
 #include <stdio.h> //include standard IO library to have access to basic functions
-
+#define SIZE 50 //define preprocessor replaces first string with value in the code
 // main function: what gets executed when you run the program
 int main(int argc, char *argv[]){ // argc - number of arguments, argv array of command line arguments
-    if(argc > 1){
-        printf("Hello %s!",argv[1]); //prints hello argv[1] to the standard output stream
-    }
-    else {
-        printf("No additional command line arguments");
-    }
+    /*
+    Variable delaration
+    */
+    int myint;
+    float myfloat;
+    char mychar;
+    double mydouble;
+    char mystring[SIZE];
+
+    /*input samples*/
+    printf("Enter integer: ");
+    scanf("%d",&myint);
+    printf("Enter float: ");
+    scanf("%f",&myfloat);
+    printf("Enter double: ");
+    scanf("%lf",&mydouble);
+    printf("Enter char: ");
+    scanf("\n%c",&mychar);
+    printf("Enter string: ");
+    scanf("\n%s",mystring);
+    //scanf("\n%[^\n]",mystring);
+
+    /*output samples*/
+    printf("your integer: %d \n",myint);
+    printf("your float: %f \n",myfloat);
+    printf("your double: %lf \n",mydouble);
+    printf("your char: %c \n",mychar);
+    printf("your string: %s \n",mystring);
     
     return 0; //returns '0', indicating successful exit of program
 }
